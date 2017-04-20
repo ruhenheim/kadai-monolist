a271a84a5546ee45eaec30e4ed181e5f48849ebb
ruhenheim <asd.uchiyama@gmail.com>
2017-04-20 18:42:57 +0900

    set preparation

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,10 @@ Bundler.require(*Rails.groups)
 
 module Monolist
   class Application < Rails::Application
+    config.i18n.available_locales = %i(ja)
+    config.i18n.enforce_available_locales = true
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
