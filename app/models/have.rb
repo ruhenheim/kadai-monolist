5c5c3cd6b51c45dde41336a29d6bcd66da3cc4d5
ruhenheim <asd.uchiyama@gmail.com>
2017-06-04 21:36:12 +0900

    ranking

--- app/models/have.rb
+++ app/models/have.rb
@@ -1,2 +1,7 @@
 class Have < Ownership
+  class << self
+    def ranking
+      group(:item_id).order('count_item_id DESC').limit(10).count(:item_id)
+    end
+  end
 end
--- app/models/want.rb
+++ app/models/want.rb
@@ -1,2 +1,7 @@
 class Want < Ownership
+  class << self
+    def ranking
+      group(:item_id).order('count_item_id DESC').limit(10).count(:item_id)
+    end
+  end
 end
